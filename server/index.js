60b6a967666fdd341ba708993f93703cbdbe77ab
NoriX <versatility.95@mail.ru>
2019-04-01 18:39:16 +0300

    logging (morgan), json body parser, autorestart (nodemon)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const morgan = require('morgan'); //logging framework
 const app = express();
 
 // App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server Setup
 const port = process.env.PORT || 3090;
