09874ba377884769f2a7a006804de6c774cae780
NoriX <versatility.95@mail.ru>
2019-04-02 17:51:24 +0300

    Creating JwtStrategy, checking Auth before entering root route

--- server/router.js
+++ server/router.js
@@ -1,4 +1,12 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+//session: false means that we don`t want to create cookie session
+const requireAuth = passport.authenticate('jwt', { session: false });
 module.exports = (app) => {
+    app.get('/', requireAuth, (req, res) => {
+        res.send({ content: 'Secret Page' });
+    })
     app.post('/signup', Authentication.signup);
 }
