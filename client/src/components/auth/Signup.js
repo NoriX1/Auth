4d14cf261b0724dbdc0ef59450928c5e9578dd90
NoriX <versatility.95@mail.ru>
2019-04-03 12:28:31 +0300

    Signing Up, Displaying Errors, Redirecting on Success

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -7,7 +7,9 @@ import * as actions from '../../actions';
 class Signup extends React.Component {
 
     onSubmit = (formProps) => {
-        this.props.signup(formProps);
+        this.props.signup(formProps, () => {
+            this.props.history.push('/feature');
+        });
     };
 
     render() {
@@ -23,15 +25,19 @@ class Signup extends React.Component {
                     <label>Password</label>
                     <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
+                <div>{this.props.errorMessage}</div>
                 <button>Sign Up</button>
             </form>
         );
     }
 }
 
+const mapStateToProps = (state) => {
+    return { errorMessage: state.auth.errorMessage };
+}
 
 // using compose to apply multiple HOC
 export default compose(
-    connect(null, actions),
+    connect(mapStateToProps, actions),
     reduxForm({ form: 'signup' })
 )(Signup);
